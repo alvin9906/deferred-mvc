c6842ccf3a0620c402073e0e3500d07229b079bb
alvin.chew <alvin.chew@no-email.com>
2018-02-06 00:55:52 +0000

    Add Rx method

---
+++ src/main/groovy/com/battletech/maddog/poc/mvc/controller/HelloRxController.groovy
@@ -0,0 +1,7 @@
+package com.battletech.maddog.poc.mvc.controller
+
+/**
+ * Created by alvin on 06/02/18.
+ */
+class HelloRxController {
+}
---
+++ src/main/groovy/com/battletech/maddog/poc/mvc/facade/HelloRxFacade.groovy
@@ -0,0 +1,7 @@
+package com.battletech.maddog.poc.mvc.facade
+
+/**
+ * Created by alvin on 06/02/18.
+ */
+class HelloRxFacade {
+}
